4e4f6aa85e075b969ee5c522d05162cc842aa6b7
Dhanush Sairam <dhanushsairam@iacg.info>
2025-12-26 18:38:14 +0530

    Game Over Score Update Bug Fixed
    Also fixed a bug in the tutorial panel

--- HalveMyCake/Assets/Scripts/Panel&UI/GameOverPanel.cs
+++ HalveMyCake/Assets/Scripts/Panel&UI/GameOverPanel.cs
@@ -22,28 +22,32 @@ public class GameOverPanel : MonoBehaviour
         if (SaveDataHandler.Instance.levelData.Count > SaveDataHandler.Instance.SaveData.unlockedLevels)
         {
             levelData = SaveDataHandler.Instance.levelData[GameSDKSystem.Instance.currentLevel];
-            if (GameSDKSystem.Instance.currentLevel == SaveDataHandler.Instance.SaveData.unlockedLevels)
+            //if (GameSDKSystem.Instance.currentLevel == SaveDataHandler.Instance.SaveData.unlockedLevels)
+            //{
+
+            //}
+            if (levelData.starThresholds[0] <= points) SaveDataHandler.Instance.SaveData.unlockedLevels += 1;
+            int i = 0;
+            //int localStarsEarned = SaveDataHandler.Instance.SaveData.starsEarned[GameSDKSystem.Instance.currentLevel];
+            while (levelData.starThresholds[i] <= points)
             {
-                if (levelData.starThresholds[0] <= points) SaveDataHandler.Instance.SaveData.unlockedLevels += 1;
-                int i = 0;
-                while (levelData.starThresholds[i] <= points)
-                {
-                    if (SaveDataHandler.Instance.SaveData.starsEarned == null)
-                    {
-                        SaveDataHandler.Instance.SaveData.starsEarned = new System.Collections.Generic.List<int>();
-                    }
-                    if(SaveDataHandler.Instance.SaveData.starsEarned.Count <= GameSDKSystem.Instance.currentLevel)
-                    {
-                        SaveDataHandler.Instance.SaveData.starsEarned.Add(new int());
-                    }
-                    SaveDataHandler.Instance.SaveData.starsEarned[GameSDKSystem.Instance.currentLevel] =
-                        SaveDataHandler.Instance.SaveData.starsEarned[GameSDKSystem.Instance.currentLevel] >= 3 ? 3 :
-                        SaveDataHandler.Instance.SaveData.starsEarned[GameSDKSystem.Instance.currentLevel] + 1;
-                    i++;
-                    if (i >= 3) break;
-                }
-                SaveDataHandler.Instance.WriteDataToSaveFile(SaveDataFiles.SaveData);
+                i++;
+                //localStarsEarned++;
+                if (i >= 3) break;
             }
+            if (SaveDataHandler.Instance.SaveData.starsEarned == null)
+            {
+                SaveDataHandler.Instance.SaveData.starsEarned = new System.Collections.Generic.List<int>();
+            }
+            if (SaveDataHandler.Instance.SaveData.starsEarned.Count <= GameSDKSystem.Instance.currentLevel)
+            {
+                SaveDataHandler.Instance.SaveData.starsEarned.Add(new int());
+            }
+            if (SaveDataHandler.Instance.SaveData.starsEarned[GameSDKSystem.Instance.currentLevel] < i)
+            {
+                SaveDataHandler.Instance.SaveData.starsEarned[GameSDKSystem.Instance.currentLevel] = i;
+            }
+            SaveDataHandler.Instance.WriteDataToSaveFile(SaveDataFiles.SaveData);
             ScoreSliderFill(levelData, points);
         }
     }
