786288e8d764edde5d9f706607b2cdfe094d5907
Dhanush Sairam <dhanushsairam@iacg.info>
2025-12-26 17:21:21 +0530

    Fixed UI bugs in Help panel

---
+++ HalveMyCake/Assets/Scripts/HelpPanel.cs
@@ -0,0 +1,11 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HelpPanel : MonoBehaviour
+{
+    [SerializeField] Button button;
+    public void GetHelp(ProblemType problem1, ProblemType problem2, OperationType operation)
+    {
+        
+    }
+}
